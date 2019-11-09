0821ccb21d6f742a7a4a352f3fb2856348096420
leemutnam <admantal8@gmail.com>
2019-11-09 17:15:41 +0900

    특정 컬럼이 있을 경우 시트를 별도의 엑셀파일로 분리하여 저장한다

---
+++ README.md
@@ -0,0 +1,11 @@
+목적
+- 엑셀 노가다를 코드로 처리하기
+
+필수 패키지
+- Virtualenv
+- python 3.7
+
+의존성 패키지
+- pandas
+- xlrd
+- openpyxl
---
+++ splitting_sheet/index.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import os
+
+project_dir_path = os.path.dirname(os.path.abspath(__file__))
+origin_dir_name = '/origin'
+origin_dir_path = os.path.abspath(''.join([project_dir_path, origin_dir_name]))
+result_dir_name = '/result'
+result_dir_path = os.path.abspath(''.join([project_dir_path, result_dir_name]))
+
+origin_file_list = os.listdir(origin_dir_path)
+latest_idx = len(origin_file_list) - 1
+origin_file_name = origin_file_list[latest_idx]
+
+origin_file_path = ''.join([origin_dir_path, '/', origin_file_name])
+xl = pd.ExcelFile(origin_file_path)
+sheet_names = xl.sheet_names
+
+addr_keys = ['주소', '지번주소', '도로명주소']
+file_type = '.xlsx'
+
+for name in sheet_names:
+    data = pd.read_excel(origin_file_path, sheet_name=name)
+    data_columns = data.columns
+    keys_existence = False
+
+    for key in addr_keys:
+        if key in data_columns:
+            keys_existence = True
+
+    if keys_existence:
+        result_file = ''.join([result_dir_path, '/', name, file_type])
+        print(result_file)
+        data.to_excel(result_file, sheet_name='Sheet1')
